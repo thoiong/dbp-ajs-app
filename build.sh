dd656f82ab7d90329cdf2be65d8b515c8b5a1cf3
thoi <thoiong@gmail.com>
2018-07-08 23:47:59 +1000

    enhance build script to cater for CI tool call to build ops/functions

--- build.sh
+++ build.sh
@@ -4,14 +4,48 @@
 #
 #==============================
 
-tidyUp () {
-    if [ -d ${app} ]
+usage () {
+    echo "Usage: $0 [${usage_known_ops}]"
+    echo "   if operation not provided, do full build process"
+    echo ""
+    echo ""
+    exit 1
+}
+
+
+isKnownOps () {
+    if [ -z "${OP}" ]
     then
-        rm -rf ${app}
+        OP=all
+    else
+        echo "==== checking if ${OP} in the known ops list ...."
+        for oper in ${known_ops}
+        do
+            if [ "${oper}" == "${OP}" ]
+            then
+                return 0
+	fi
+        done
+        echo "==== "
+	echo "==== Unknown operation.  Exit"
+        echo "==== "
+        echo " "
+	usage
+    fi
+
+}
+
+tidyUp () {
+    echo "tidyup build workspace ...."
+    if [ -d ${app_dir} ]
+    then
+        echo "====  deleting ${app_dir} dir ...."
+        rm -rf ${app_dir}
     fi
 
     if [ -d ${seed_proj_dir} ]
     then
+        echo "====  deleting ${seed_proj_dir} dir ...."
         rm -rf ${seed_proj_dir}
     fi
 }
@@ -25,6 +59,12 @@ buildBuilderImg () {
 }
 
 getAppFromBuilderImg () {
+    isBuildCont=$(docker ps -a|grep "${build_cont_name}")
+    if [ ! -z "${isBuildCont}" ]
+    then
+        docker rm ${build_cont_name}
+    fi
+
     docker create --name ${build_cont_name} ${build_img_name}
     docker cp ${build_cont_name}:/build-workspace/${app_dir} .
 }
@@ -33,9 +73,29 @@ buildApp () {
     docker build -t ${app_img_name} .
 }
 
+all () {
+    tasks="tidyUp getAngularSeedCodes buildBuilderImg getAppFromBuilderImg buildApp"
+    for task in ${known_ops}
+    do
+        ${task}
+	rc=$?
+	if [ $? -ne 0 ]
+	then
+	    echo "${task} return non ${rc}"
+	    exit ${rc}
+	fi
+    done
+
+
+}
 #=========
 # main
 #=========
+OP=$1
+
+known_ops="tidyUp getAngularSeedCodes buildBuilderImg getAppFromBuilderImg buildApp"
+usage_known_ops="tidyUp|getAngularSeedCodes|buildBuilderImg|getAppFromBuilderImg|buildApp"
+
 angular_seed_src_url="https://github.com/angular/angular-seed.git"
 seed_proj_dir="angular-seed"
 app_dir=app
@@ -43,10 +103,5 @@ build_img_name="build-img"
 build_cont_name="build-cont"
 app_img_name="angular-seed-app-img"
 
-
-tidyUp
-getAngularSeedCodes
-buildBuilderImg
-getAppFromBuilderImg
-buildApp
-
+isKnownOps
+${OP}
